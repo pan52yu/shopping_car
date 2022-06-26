122319b4eb9806f93b83fac197de7864620d5fcf
itHy <1324876445@qq.com>
2022-06-26 17:07:52 +0800

    完成页面组件初始化

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <MyHeader></MyHeader>
+    <MyGoods></MyGoods>
+    <MyFooter></MyFooter>
+  </div>
 </template>
 
 <script>
+import MyHeader from './components/MyHeader'
+import MyGoods from './components/MyGoods'
+import MyFooter from './components/MyFooter'
 export default {
-  name: 'App'
+  name: 'App',
+  components: { MyHeader, MyGoods, MyFooter }
 }
 </script>
 
-<style lang="less"></style>
+<style lang="less">
+#app {
+  padding: 50px 0;
+}
+</style>

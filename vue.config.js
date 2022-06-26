831884fa89a119dd9d1d63d4b25f4070fa0802f9
itHy <1324876445@qq.com>
2022-06-26 17:00:31 +0800

    初始化仓库，并完成配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: false, // 关闭语法检查 // 写原生webpack的配置项
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 10086,
+      host: 'localhost'
+    }
+  }
 })
